405a7261a3ff27626b02ab9b44b79074a43cd74f
Vinicius Menis <74736432+ResT-0@users.noreply.github.com>
2023-03-05 21:39:07 -0300

    Add files via upload

---
+++ ASR4/formulario.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $('#Enviar').click(function(){
+        if($("#nome").val() == "" || $("#Login").val() == "" || $("#senha").val() == ""){
+            alert("Preencha os campos Nome, Login e Senha");
+            return;
+        }
+        if($("input[name='salario']:checked").val() == undefined){
+            alert("Selecione uma opção de salário");
+            return;
+        }
+        if($("input[name='linguagem']:checked").val() == undefined){
+            alert("Selecione uma opção de linguagem");
+            return;
+        }
+        if($("#Foto").val() == ""){
+            alert("Preencha o campo foto");
+            return;
+        }
+
+        var formData = {
+        nome: nome,
+        login: login,
+        senha: senha,
+        moeda: $("input[name='salario']:checked").val(),
+        ling: $("input[name='linguagem']:checked").map(function() {
+            return $(this).val();
+        }).get(),
+        assuntos: $("select[name='assuntos']").val(),
+        comentarios: $("textarea[name='comentarios']").val()
+        };
+        $.ajax({
+        url: "arquivo.php", //nome aleátorio para o arquivo
+        type: "POST",
+        data: JSON.stringify(formData),
+        contentType: "application/json",
+        success: function(data) {
+            alert("Dados enviados com sucesso!");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Erro ao enviar os dados: " + errorThrown);
+        }
+        });
+    })
+    $("#Limpar").click(function(){
+        $("#nome").val("");
+        $("#Login").val("");
+        $("#senha").val("");
+        $("input[name='salario']").prop("checked", false);
+        $("input[name='linguagem']").prop("checked", false);
+        $("select[name='assuntos']").val("");
+        $("textarea[name='comentario']").val("");
+        $("#Foto").val(""); 
+        alert("Dados limpos com sucesso!")
+    });
+});
